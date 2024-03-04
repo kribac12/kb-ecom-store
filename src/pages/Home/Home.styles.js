c6b073a65fd05ec373bf479d3a7c8d06ead41de3
kribac12 <89196018+kribac12@users.noreply.github.com>
2024-03-04 14:24:13 +0100

    Added heroimage

---
+++ src/pages/Home/Home.styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HeroImage = styled.div`
+  background-image: url(${({ image }) => image});
+  background-position: center;
+  background-size: cover;
+  height: 40vh;
+  width: 100%;
+  position: relative;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  margin-top: 20px;
+`;
+
+export const ProductCardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
